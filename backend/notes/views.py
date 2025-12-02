f144fa1b5d5b3a59d11b49243b62e267fdc0b86e
kasyapgnath <kasyapgnath97@gmail.com>
2025-12-02 15:31:34 +0530

    second

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
 from .models import NoteInfo
 from .serializers import NoteSerializer
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated     # ⭐ Add this
+
+# ViewSet automatically provides GET and POST (and more)
 
 class NoteViewSet(viewsets.ModelViewSet):
-    queryset=NoteInfo.objects.all()
-    serializer_class=NoteSerializer
+#     queryset = NoteInfo.objects.all()   DELETE THIS LINE
+    
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]   # ⭐ Add this
+
+    def get_queryset(self):
+        return NoteInfo.objects.filter(user=self.request.user)  # ⭐ Only their notes
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)  # ⭐ Automatically set user
